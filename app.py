2fb7b1c9f38cb0516f30f14f09e586e90d642ffb
Francisco de Assis Boldt <fboldt@gmail.com>
2021-01-11 19:19:43 -0300

    redirection

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return redirect("/hello")
+
+@app.route("/hello")
 def hello_world():
     return 'Hello, Heroku!'
