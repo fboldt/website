5936d8e1571ccd1742dd086529ea04d59a374058
Francisco de Assis Boldt <fboldt@gmail.com>
2021-01-26 16:50:00 -0300

    check if csv file is bigger than numclusters

--- makeclustersfile.py
+++ makeclustersfile.py
@@ -1,16 +1,20 @@
 import pandas as pd
 from sklearn.cluster import KMeans
-from os.path import splitext, basename
 import numpy as np
 
 def makeclustersfile(filename):
+    numclusters = 100
     df = pd.read_csv(filename)
     latlng = df[['metadata.lat','metadata.lng']]
     X = latlng.to_numpy()
-    kmeans = KMeans(100)
-    y_pred = kmeans.fit_predict(X)
-    _, counts = np.unique(y_pred, return_counts=True)
-    df = pd.DataFrame(kmeans.cluster_centers_, columns=['longitude','latitude'])
+    if len(X) > numclusters:
+        kmeans = KMeans(numclusters)
+        y_pred = kmeans.fit_predict(X)
+        _, counts = np.unique(y_pred, return_counts=True)
+        df = pd.DataFrame(kmeans.cluster_centers_, columns=['longitude','latitude'])
+    else:
+        df = pd.DataFrame(X, columns=['longitude','latitude'])
+        counts = np.ones(len(latlng))
     df['counts'] = counts
     return df.to_json()
     
