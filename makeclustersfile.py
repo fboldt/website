e5a5d67a4a286e853cde3473f0fe573beca0ccf5
Francisco de Assis Boldt <fboldt@gmail.com>
2021-01-27 13:07:52 -0300

    fiz lat lng

--- makeclustersfile.py
+++ makeclustersfile.py
@@ -5,15 +5,15 @@ import numpy as np
 def makeclustersfile(filename):
     numclusters = 100
     df = pd.read_csv(filename)
-    latlng = df[['metadata.lat','metadata.lng']]
+    latlng = df[['metadata.lng','metadata.lat']]
     X = latlng.to_numpy()
     if len(X) > numclusters:
         kmeans = KMeans(numclusters)
         y_pred = kmeans.fit_predict(X)
         _, count = np.unique(y_pred, return_counts=True)
-        df = pd.DataFrame(kmeans.cluster_centers_, columns=['lat','lng'])
+        df = pd.DataFrame(kmeans.cluster_centers_, columns=['lng','lat'])
     else:
-        df = pd.DataFrame(X, columns=['lat','lng'])
+        df = pd.DataFrame(X, columns=['lng','lat'])
         count = np.ones(len(latlng))
     df['count'] = count
     return df.to_json(orient='records')
